f47aab5d8a9f291a913b2ad45d9373329cb6775c
= <sanghun0724@naver.com>
2023-04-08 19:55:01 +0900

    [v1.0.0][ 🌟- project 추가 ]

---
+++ omos/omos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  omos
+//
+//  Created by sangheon on 2023/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World")
+    }
+
+
+}
+
