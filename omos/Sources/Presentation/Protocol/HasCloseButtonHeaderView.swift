14c7e201e15dee6dfaf6461e3f6fdc5cdf689851
= <sanghun0724@naver.com>
2023-05-11 22:15:14 +0900

    [v1.0.0][ 🛠- 기타 파일 수정 ]

--- omos/Sources/Presentation/Protocol/HasCloseButtonHeaderView.swift
+++ omos/Sources/Presentation/Protocol/HasCloseButtonHeaderView.swift
@@ -8,27 +8,27 @@
 import UIKit
 import SnapKit
 
-protocol HasCloseButtonHeaderView {
-    var needHeaderView: Bool { get }
-    var headerView: CloseButtonHeaderView { get }
-}
-
-extension HasCloseButtonHeaderView where Self: UIViewController {
-    var needHeaderView: Bool {
-        navigationController == nil
-    }
-    
-    func addHeaderViewIfNeeded(to view: UIView) {
-        guard self.needHeaderView else { return }
-        view.addSubview(headerView)
-    }
-    
-    func makeHeaderViewConstraintsIfNeeded() {
-        guard self.needHeaderView else { return }
-        headerView.snp.makeConstraints {
-            $0.height.equalTo(52)
-            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            $0.leading.trailing.equalToSuperview()
-        }
-    }
-}
+//protocol HasCloseButtonHeaderView {
+//    var needHeaderView: Bool { get }
+//    var headerView: CloseButtonHeaderView { get }
+//}
+//
+//extension HasCloseButtonHeaderView where Self: UIViewController {
+//    var needHeaderView: Bool {
+//        navigationController == nil
+//    }
+//    
+//    func addHeaderViewIfNeeded(to view: UIView) {
+//        guard self.needHeaderView else { return }
+//        view.addSubview(headerView)
+//    }
+//    
+//    func makeHeaderViewConstraintsIfNeeded() {
+//        guard self.needHeaderView else { return }
+//        headerView.snp.makeConstraints {
+//            $0.height.equalTo(52)
+//            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+//            $0.leading.trailing.equalToSuperview()
+//        }
+//    }
+//}
