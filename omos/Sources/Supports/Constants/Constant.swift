3c3d9e730c754def32b246cbd8aee52e787d1a8a
= <sanghun0724@naver.com>
2023-05-13 17:16:03 +0900

    [v1.0.0][ 🌟- LoginHeaderView 추가 ]

---
+++ omos/Sources/Supports/Constants/Constant.swift
@@ -0,0 +1,16 @@
+//
+//  Constant.swift
+//  omos
+//
+//  Created by sangheon on 2023/05/13.
+//
+
+import UIKit
+
+enum CommonUI {
+    static let loginCorner = 4.0
+    static let LoginTopViewHeight = 0.6
+    static let mainHeight = UIScreen.main.bounds.height
+    static let mainWidth = UIScreen.main.bounds.width
+    static var statuBarHeight: CGFloat = 0.0
+}
