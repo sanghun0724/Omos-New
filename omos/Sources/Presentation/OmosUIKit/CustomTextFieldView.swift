1e5f7b0d4c2465271dd82a1cd06434a7cb29ffa9
= <sanghun0724@naver.com>
2023-05-14 00:11:16 +0900

    [v1.0.0][ 🌟- CustomTextField 추가 ]
    UI

---
+++ omos/Sources/Presentation/OmosUIKit/CustomTextFieldView.swift
@@ -0,0 +1,87 @@
+//
+//  CustomTextFieldView.swift
+//  omos
+//
+//  Created by sangheon on 2023/05/13.
+//
+
+import UIKit
+import SnapKit
+
+class CustomTextFieldView: BaseView {
+    
+    // MARK: - Constants
+
+    private enum UI {
+      static let textFieldHeight: CGFloat = 48
+    }
+    
+    lazy var textField: CustomTextField = {
+        let textField = CustomTextField()
+        return textField
+    }()
+    
+    lazy var leftTopLabel: UILabel = {
+        let label = UILabel()
+        label.isHidden = false
+        return label
+    }()
+    
+    lazy var rightTopLabel: UILabel = {
+        let label = UILabel()
+        label.isHidden = false
+        label.textColor = .orange
+        return label
+    }()
+    
+    func fetchLeftTopLabelText(text: String) {
+        leftTopLabel.text = text
+    }
+    
+    func fetchRightTopLabelText(text: String) {
+        rightTopLabel.text = text
+    }
+    
+    func setWarningState() {
+        print("setWarningState")
+    }
+    
+    func setNomalState() {
+        print("setNomalState")
+    }
+    
+    override func initialize() {
+        super.initialize()
+        addSubview(textField)
+        addSubview(leftTopLabel)
+        addSubview(rightTopLabel)
+    }
+    
+    override func setupConstraints() {
+        super.setupConstraints()
+        self.makeLeftTopLabelConstraints()
+        self.makeRightTopLabelConstraints()
+        self.makeTextFieldConstraints()
+    }
+    
+    private func makeLeftTopLabelConstraints() {
+        leftTopLabel.snp.makeConstraints {
+            $0.left.top.equalToSuperview()
+        }
+    }
+    
+    private func makeRightTopLabelConstraints() {
+        rightTopLabel.snp.makeConstraints {
+            $0.right.top.equalToSuperview()
+        }
+    }
+    
+    private func makeTextFieldConstraints() {
+        textField.snp.makeConstraints {
+            $0.height.equalTo(UI.textFieldHeight)
+            $0.top.equalTo(rightTopLabel.snp.bottom).offset(10)
+            $0.left.right.bottom.equalToSuperview()
+        }
+    }
+    
+}
