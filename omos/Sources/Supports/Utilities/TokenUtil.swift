342ca4fc780ca2bcaa8c5db0fceb13551f435248
= <sanghun0724@naver.com>
2023-05-20 14:19:37 +0900

    [v1.0.0][ 🌟- Key chain Token Util 추가 ]

---
+++ omos/Sources/Supports/Utilities/TokenUtil.swift
@@ -0,0 +1,84 @@
+//
+//  TokenUtil.swift
+//  omos
+//
+//  Created by sangheon on 2023/05/20.
+//
+
+import Foundation
+
+import Security
+import Alamofire
+ 
+class TokenUtils {
+    
+    // Create
+ // service 파라미터는 url주소를 의미
+    func create(_ service: String, account: String, value: String) {
+        
+        // 1. query작성
+        let keyChainQuery: NSDictionary = [
+            kSecClass : kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecValueData: value.data(using: .utf8, allowLossyConversion: false)!
+        ]
+        // allowLossyConversion은 인코딩 과정에서 손실이 되는 것을 허용할 것인지 설정
+        
+        // 2. Delete
+        // Key Chain은 Key값에 중복이 생기면 저장할 수 없기때문에 먼저 Delete
+        SecItemDelete(keyChainQuery)
+        
+        // 3. Create
+        let status: OSStatus = SecItemAdd(keyChainQuery, nil)
+        assert(status == noErr, "failed to saving Token")
+    }
+    
+    // Read
+    func read(_ service: String, account: String) -> String? {
+        let KeyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecReturnData: kCFBooleanTrue, // CFData타입으로 불러오라는 의미
+            kSecMatchLimit: kSecMatchLimitOne // 중복되는 경우 하나의 값만 가져오라는 의미
+        ]
+        // CFData 타입 -> AnyObject로 받고, Data로 타입변환해서 사용하면됨
+        
+        // Read
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(KeyChainQuery, &dataTypeRef)
+        
+        // Read 성공 및 실패한 경우
+        if(status == errSecSuccess) {
+            let retrievedData = dataTypeRef as! Data
+            let value = String(data: retrievedData, encoding: String.Encoding.utf8)
+            return value
+        } else {
+            print("failed to loading, status code = \(status)")
+            return nil
+        }
+    }
+    
+    // Delete
+    func delete(_ service: String, account: String) {
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account
+        ]
+        
+        let status = SecItemDelete(keyChainQuery)
+        assert(status == noErr, "failed to delete the value, status code = \(status)")
+    }
+    
+    // HTTPHeaders 구성
+    func getAuthorizationHeader(serviceID: String) -> HTTPHeaders? {
+        let serviceID = serviceID
+        if let accessToken = self.read(serviceID, account: "accessToken") {
+            return ["Authorization" : "bearer \(accessToken)"] as HTTPHeaders
+        } else {
+            return nil
+        }
+    }
+}
