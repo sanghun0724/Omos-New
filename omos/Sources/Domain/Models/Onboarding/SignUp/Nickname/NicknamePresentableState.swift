28a6b4d6d5bc91a51ad142d57bbb88730d7bb595
= <sanghun0724@naver.com>
2023-06-10 15:23:38 +0900

    [v1.0.0][ 🌟- Nickname 리블렛 추가 ]

---
+++ omos/Sources/Domain/Models/Onboarding/SignUp/Nickname/NicknamePresentableState.swift
@@ -0,0 +1,12 @@
+//
+//  NicknamePresentableState.swift
+//  omos
+//
+//  Created by sangheon on 2023/06/10.
+//
+
+import Foundation
+
+struct NicknamePresentableState {
+    
+}
---
+++ omos/Sources/Presentation/Scene/OnBoarding/SignUp/Nickname/View/AgreementView.swift
@@ -0,0 +1,21 @@
+//
+//  AgreementView.swift
+//  omos
+//
+//  Created by sangheon on 2023/06/10.
+//
+
+import UIKit
+
+class AgreementView: BaseView {
+    
+    // MARK: - UI Components
+    
+    private lazy var checkCircleView = UIButton().builder
+        .set(\.layer.masksToBounds, to: true)
+        .build()
+    
+    private lazy var 
+}
+
+
