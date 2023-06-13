8d1c0ade4744ffdbb710037e1008e33ba4551f7c
= <sanghun0724@naver.com>
2023-06-13 20:47:52 +0900

    [v1.0.0][ 🌟 - 외부 의존성 추가 ]

--- Projects/Shared/GlobalThirdPartyLibrary/Project.swift
+++ Projects/Shared/GlobalThirdPartyLibrary/Project.swift
@@ -5,5 +5,24 @@ import DependencyPlugin
 let project = Project.makeModule(
     name: ModulePaths.Shared.GlobalThirdPartyLibrary.rawValue,
     product: .framework,
-    targets: []
+    targets: [],
+    externalDependencies: [
+        .SPM.RxSwift,
+        .SPM.RxCocoa,
+        .SPM.LogFlume,
+        .SPM.RIBs,
+        .SPM.RxDataSources,
+        .SPM.NeedleFoundation,
+        .SPM.ReactorKit,
+        .SPM.Kingfisher,
+        .SPM.Alamofire,
+        .SPM.SkeletonView,
+        .SPM.SnapKit,
+        .SPM.KakaoSDK,
+        .SPM.RxGesture,
+        .SPM.MyConstants
+    ],
+    internalDependencies: [
+        .shared(target: .Utilities)
+    ]
 )
