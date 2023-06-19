dfd214feb547a3c60999a20be27ef1df245e105a
= <sanghun0724@naver.com>
2023-06-19 09:36:48 +0900

    [v1.0.0][ 🛠 - Module: 모듈간 의존성 변경   ]
    그래프 변경

--- Projects/Domain/OnboardingDomain/Project.swift
+++ Projects/Domain/OnboardingDomain/Project.swift
@@ -6,7 +6,5 @@ let project = Project.makeModule(
     name: ModulePaths.Domain.OnboardingDomain.rawValue,
     product: .staticLibrary,
     targets: [.interface, .testing, .unitTest],
-    internalDependencies: [
-        .domain(target: .BaseDomain),
-    ]
+    internalDependencies: []
 )
