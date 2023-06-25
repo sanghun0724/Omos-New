57f359179b7bb3ef54b659813dc9c01ca96660c7
shlee <shlee@autocrypt.io>
2023-06-25 17:32:07 +0900

    [v1.0.0][ 🛠 - Config 변경  ]

--- Projects/Core/CoreKit/Sources/Platform/DataSource/APIKeys.swift
+++ Projects/Core/CoreKit/Sources/Platform/DataSource/APIKeys.swift
@@ -10,9 +10,9 @@ import Foundation
 
 public struct RestApiUrl {
 #if DEBUG
-    public static let debugUrl = ""
+    public static let debugUrl = Environment.rootURL.absoluteString
 #else
-    public static let restUrl = ""
+    public static let restUrl = Environment.rootURL.absoluteString
 #endif
 }
 
@@ -22,5 +22,43 @@ public struct AWS {
 }
 
 public struct KAKAO {
-    public static let Key = ""
+    public static let Key = Environment.kakaoAPIKey
+}
+
+
+
+public enum Environment {
+    // MARK: - Keys
+    enum Keys {
+        enum Plist {
+            static let rootURL = "ROOT_URL"
+            static let kakaoAPIKey = "KAKAO_API_KEY"
+        }
+    }
+
+    // MARK: - Plist
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("Plist file not found")
+        }
+        return dict
+    }()
+
+    // MARK: - Plist values
+    public static let rootURL: URL = {
+        guard let rootURLstring = Environment.infoDictionary[Keys.Plist.rootURL] as? String else {
+            fatalError("Root URL not set in plist for this environment")
+        }
+        guard let url = URL(string: rootURLstring) else {
+            fatalError("Root URL is invalid")
+        }
+        return url
+    }()
+
+    public static let kakaoAPIKey: String = {
+        guard let apiKey = Environment.infoDictionary[Keys.Plist.kakaoAPIKey] as? String else {
+            fatalError("API Key not set in plist for this environment")
+        }
+        return apiKey
+    }()
 }
