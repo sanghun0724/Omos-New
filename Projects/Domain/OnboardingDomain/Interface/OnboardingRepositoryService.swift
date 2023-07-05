7c23bb693d0e4e52dd000f955b96bd1473cf4f9b
sangheon <sanghun0724@naver.com>
2023-07-05 21:57:41 +0900

    [v1.0.0][ 🛠 - usecase - appleLogin interface 삭제  ]

--- Projects/Domain/OnboardingDomain/Interface/OnboardingRepositoryService.swift
+++ Projects/Domain/OnboardingDomain/Interface/OnboardingRepositoryService.swift
@@ -7,7 +7,6 @@ public protocol OnboardingRepositoryService {
     func login(email: String, password: String) -> Observable<Bool>
     func signUp() -> Observable<Bool>
     func kakaoLogin() -> Observable<String>
-    func appleLogin() -> Observable<String>
     func checkEmailDuplication(email: String) -> Observable<Bool>
     func requestAuthEmailCode(email: String) -> Observable<Bool>
     func isValidEmail(email: String) -> Observable<Bool>
